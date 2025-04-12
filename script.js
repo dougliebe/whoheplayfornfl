5932e71af7ca9bb53eefb39169691509ed1343e8
dougliebe <liebe.doug@gmail.com>
2025-04-12 14:33:13 -0500

    currently not working changes to get player images

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const playerNameElement = document.getElementById('player-name');
+    const playerImageElement = document.getElementById('player-image');
     const actionButton = document.getElementById('action-button');
     let currentPlayerIndex = 0;
     let players = [];
@@ -22,6 +23,22 @@ document.addEventListener('DOMContentLoaded', () => {
     function displayPlayer(index) {
         playerNameElement.textContent = players[index].name;
         actionButton.textContent = "Where'd He Play In College?";
+        fetchPlayerImage(players[index].name, 'nfl');
+    }
+
+    function fetchPlayerImage(playerName, context) {
+        const query = `${playerName} ${context}`;
+        const url = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(query)}&searchType=image&key=244607930052-i8qcpfmfh5bisfcilj614o1gpt21vilh.apps.googleusercontent.com&cx=1372ec7bbff2943c2`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const imageUrl = data.items[0].link;
+                playerImageElement.src = imageUrl;
+                playerImageElement.style.display = 'block';
+            })
+            .catch(() => {
+                playerImageElement.style.display = 'none';
+            });
     }
 
     function getRandomPlayerIndex() {
@@ -36,6 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (actionButton.textContent === "Where'd He Play In College?") {
             playerNameElement.textContent += ` - ${players[currentPlayerIndex].college}`;
             actionButton.textContent = 'Next Player';
+            fetchPlayerImage(players[currentPlayerIndex].name, players[currentPlayerIndex].college);
         } else {
             currentPlayerIndex = getRandomPlayerIndex();
             // log the current player index
